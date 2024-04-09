a028e92803fde19012a8973dcd21e11209512f33
은지 <81403265+eunji0714@users.noreply.github.com>
2024-04-09 15:38:58 +0900

    색션6-3

---
+++ 섹션6/3. 크레인 인형뽑기.js
@@ -0,0 +1,33 @@
+function solution(board, moves) {
+  let answer = 0
+  let stack = []
+  for (let x of moves) {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][x - 1] !== 0) {
+        stack.push(board[i][x - 1])
+        board[i][x - 1] = 0
+        break
+      }
+      for (let j = 0; j < stack.length; j++) {
+        if (stack[j] === stack[j + 1]) {
+          answer += 2
+          stack.splice(j, 2)
+          j = 0
+        }
+      }
+    }
+  }
+
+  return answer
+}
+
+let a = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+]
+
+let b = [1, 5, 3, 5, 1, 2, 1, 4]
+console.log(solution(a, b))
